e117cb6c61531fe079e1014aff93eac1f3708b2e
Sebastian Ebel <sebastianebel@hotmail.de>
2021-10-16 07:27:23 +0200

    images added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'bastiebel';
+export class AppComponent implements OnInit {
+  images = ['pexels-andrea-piacquadio-840996.jpg', 'pexels-danny-meneses-943096.jpg', 'pexels-karsten-madsen-18105.jpg'];
+
+  currenImage = 0;
+  showImage = true;
+
+  ngOnInit(){
+    this.updateImages();
+  }
+
+  updateImages() {
+    setInterval(() => {
+      this.currenImage++;
+      this.currenImage = this.currenImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
 }
